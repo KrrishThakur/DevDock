179d0810ed506851cddb2bf119b7c49cd4847651
KrrishThakur <sasukeuzuki64@gmail.com>
2026-09-10 15:01:07 +0530

    deploy: add backend/src/main/java/com/devdock/DevDockApplication.java

---
+++ backend/src/main/java/com/devdock/DevDockApplication.java
@@ -0,0 +1,11 @@
+package com.devdock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevDockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DevDockApplication.class, args);
+    }
+}
